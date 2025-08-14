daad2f65de24e56a3f8b2fe967eb71ce466c1bf5
MarwaneMLE <marwanekhadrouf91@gmail.com>
2025-08-14 08:19:29 -0700

    advanced api

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include
-from .views import (
-    BookListView,
-    BookRetrieveView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView
-)
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+
 
 urlpatterns = [
     path('books/list/', BookListView.as_view(), name = 'book-list'),
-    path('books/<int:pk>/', BookRetrieveView.as_view(), name = 'book-detail'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name = 'book-detail'),
     path('books/create/', BookCreateView.as_view(), name = 'book-create'),
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name = 'book-update'),
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name = 'book-delete'),
+    path('api-token-auth/', obtain_auth_token, name = 'api-token-auth'),
 ]
